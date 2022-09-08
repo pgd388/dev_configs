73bc4238969181e8c1425194d90b75a96985faf2
Rene <rene@CUBICLE06.local>
2022-09-08 11:52:34 -0400

    First commit.

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,14 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "-", "$", opts)
+keymap("n", " ", ":", opts)
+keymap("n", "  ", "/", opts)
+keymap("n", " qq", ":q!", opts)
+keymap("i", "qq", "<ESC>", opts)
+keymap("i", "wq", "<ESC>:w", opts)
+keymap("v", "qq", "<ESC>", opts)
+
+-- Keep original clipboard copy
+keymap("v", "p", '"_dP', opts)
