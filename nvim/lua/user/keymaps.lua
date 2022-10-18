6ea0c86b375f7b5368c898df47616efcf9078287
Rene <rene@CUBICLE06.local>
2022-10-18 13:16:20 -0400

    Add word wrap toggle shortcuts.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -2,6 +2,8 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+keymap("n", "sw", ":set wrap", opts)
+keymap("n", "nw", ":set nowrap", opts)
 keymap("n", "-", "$", opts)
 keymap("n", " ", ":", opts)
 keymap("n", "  ", "/", opts)
