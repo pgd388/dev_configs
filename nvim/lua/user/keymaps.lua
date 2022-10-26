6943a8e3da826ebf416d8400246ddf94fddb4b19
Rene <rene@CUBICLE06.local>
2022-10-26 08:44:46 -0400

    Add netrw keymap for current dir.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -2,8 +2,9 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "sw", ":set wrap", opts)
-keymap("n", "nw", ":set nowrap", opts)
+keymap("n", "sw", ":set wrap<CR>", opts)
+keymap("n", "nw", ":set nowrap<CR>", opts)
+keymap("n", "ee", ":e %:p:h/<CR>", opts)
 keymap("n", "-", "$", opts)
 keymap("n", " ", ":", opts)
 keymap("n", "  ", "/", opts)
